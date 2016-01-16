35d98a751478743d99703f7261936ae6e78048d5
Timmy2005 <tjcp2@student.k12albemarle.org>
2016-01-16 17:14:02 -0500

    i think is works mostly now

--- gameCore.py
+++ gameCore.py
@@ -19,68 +19,51 @@ global corNumberX
 global corNumberY
 
 def playerAction():
-    # This is the 'playerAction' function. It is used for typing commands.
-    global corNumberX
-    global corNumberY 
-    corNumX = str(corNumberX)
-    corNumY = str(corNumberY)
-    print("You are at X:" + corNumX + " Y:" + corNumY)
-    
-    playerInput = raw_input("> ").lower()
-    if playerInput == "north":
+	# This is the 'playerAction' function. It is used for typing commands
+	global corNumberX
+	global corNumberY
+	corNumX = str(corNumberX)
+	corNumY = str(corNumberY)
+	print("You are at X:" + corNumX + " Y:" + corNumY)
+	
+	playerInput = raw_input("> ").lower()
+	if playerInput == "north":
 		corNumberY = corNumberY + 1
-		playerAction()	
-    else:
-        print("Invalid command.")
-        playerAction()
-        	
-	if playerInput == "south":
-		corNumberY = corNumberY - 1
 		playerAction()
 	else:
-		print("Invalid command.")
-		playerAction()
-        		
-	if playerInput == "east":
-		corNumberX = corNumberX + 1
-		playerAction()
-	else:
-		print("Invalid command.")
-        playerAction()
-        	
-	if playerInput == "west":
-		corNumberX = corNumberX - 1
-		playerAction()
-	else:
-		print("Invalid command.")
-        playerAction()
-        	
-    if playerInput == "inventory":
-    	showInventory() # This function is defined in the 'inventory.py' file.
-    	playerAction()
-    else:
-    	print("Invalid command.")
-    	playerAction()    
-    
-    if playerInput == "store":
-    	store()
-    	playerAction()
-    else:
-    	print("Invalid command.")
-        playerAction()
-        
-    if playerInput == "exitgame":
-        print("Are you sure you want to quit? ('yes' or 'no')")
-        exitConfirm = raw_input("> ")
-        if exitConfirm == "yes":
-            print("Exiting the game...")
-            exit()
-        if exitConfirm == "no":
-            print("Will not exit the game.")
-            playerAction()
-    else:
-        print("Invalid command.")
-        playerAction()
+		print"HI"
+		if playerInput == "south":
+			corNumberY = corNumberY - 1
+			playerAction()
+		else:
+			if playerInput == "east":
+				corNumberX = corNumberX + 1
+				playerAction()
+			else:
+				if playerInput == "west":
+					corNumberX = corNumberX - 1
+					playerAction()
+				else:
+					if playerInput == "inventory":
+						showInventory() # This function is defined in the 'inventory.py' file.
+						playerAction()
+					else:
+						if playerInput == "store":
+							store()
+							playerAction()
+						else:
+							if playerInput == "exitgame":
+								print("Are you sure you want to quit? ('yes' or 'no')")
+								exitConfirm = raw_input("> ")
+								if exitConfirm == "yes":
+									print("Exiting the game...")
+									exit()
+								if exitConfirm == "no":
+									print("Will not exit the game.")
+									playerAction()
+								else:
+									print("Invalid command.")
+									playerAction()
     # And I'll let Timmy2005 finish the code.
 
 
