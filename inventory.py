9198607bbeb42ba6a1a4460f062cd23f59df7a93
HalfKelp <dylancom101@gmail.com>
2016-01-16 11:59:11 -0500

    Updated inventory.py

--- inventory.py
+++ inventory.py
@@ -1,24 +1,31 @@
 # This is a stats and inventory concept.
 # Written by HalfKelp.
 
-mana = 0
-health = 20
-level = 1
-xp = 0
-
-Sword = False
-BowAndArrow = False
+# I integrated stats.py into the inventory.py
+# These are the stats you start with at the beginning of the game.
+def defaultStats():
+    mana = 0
+    health = 20
+    level = 1
+    xp = 0
 
 def showInventory():
-        print("Mana:")
-        print(mana)
-        print(" ")
-        print("Health:")
-        print(health)
-        print(" ")
-        print("Level:")
-        print(level)
-        print(" ")
-        print("XP:")
-        print(xp)
-        print(" ")
+    if BowAndArrow == True:
+        print("Bow & Arrow")
+    if Sword == True:
+        print("Sword")
+
+def showStats():
+    print(" ")
+    print("Mana:")
+    print(mana)
+    print(" ")
+    print("Health:")
+    print(health)
+    print(" ")
+    print("Level:")
+    print(level)
+    print(" ")
+    print("XP:")
+    print(xp)
+    print(" ")
