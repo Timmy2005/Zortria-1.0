5bbc5802eb277096cb397ee50a912845848d6412
HalfKelp <dylancom101@gmail.com>
2016-01-15 21:04:31 -0800

    Massive_update_to_all_files

--- gameCore.py
+++ gameCore.py
@@ -1,3 +1,34 @@
-import gameStartTest
-import battleEngine
-	
+# This is the format for inputting commands:
+#
+# print("Question here. (ex. What is your name?)")
+# <variable> = raw_input("> ")
+#
+# Do it EXACTLY as shown above. Copy and paste if you need to.
+
+from gameStartTest import gameStart
+import inventory
+from inventory import showInventory
+def playerAction():
+    # This is the 'playerAction' function. It is used for typing commands.
+    playerInput = raw_input("> ")
+    if playerInput == "inventory":
+        showInventory() # This function is defined in the 'inventory.py' file.
+        playerAction()
+    if playerInput == "exitgame":
+        print("Are you sure you want to quit? ('yes' or 'no')")
+        exitConfirm = raw_input("> ")
+        if exitConfirm == "yes":
+            print("Exiting the game...")
+            exit()
+        if exitConfirm == "no":
+            print("Will not exit the game.")
+            playerAction()
+    else:
+        print("Invalid command.")
+        playerAction()
+    # And I'll let Timmy2005 finish the code.
+
+
+gameStart()
+print("Input Command. (Type 'help' for help.)")
+playerAction()
---
+++ inventory.py
@@ -0,0 +1,24 @@
+# This is a stats and inventory concept.
+# Written by HalfKelp.
+
+mana = 0
+health = 20
+level = 1
+xp = 0
+
+Sword = False
+BowAndArrow = False
+
+def showInventory():
+        print("Mana:")
+        print(mana)
+        print(" ")
+        print("Health:")
+        print(health)
+        print(" ")
+        print("Level:")
+        print(level)
+        print(" ")
+        print("XP:")
+        print(xp)
+        print(" ")
