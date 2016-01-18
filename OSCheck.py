4a7a05228a353344572404ff619f9368e59ad5fd
HalfKelp <dylancom101@gmail.com>
2016-01-17 16:32:34 -0800

    Renamed gameInit.py to OSCheck.py

--- OSCheck.py
+++ OSCheck.py
@@ -1,5 +1,6 @@
 # This is the game initalization module.
-# Name self-explanatory.
+# It checks the user's OS. (Windows, OS X, etc.)
+# I just made this in case we ever need to use it.
 # Written by HalfKelp.
 
 import os
@@ -12,7 +13,6 @@ def computerCheck():
     print ("OS:                     " + OS)
     print ("OS Release:             " + OSRelease)
     print ("OS Name:                " + OSName)
-    print (" ")
     print (" ")
     print (" ")
     print (" ")
