0b4df2f48ba10973b5d4270d1023b87be1f82d14
HalfKelp <dylancom101@gmail.com>
2016-01-31 19:27:23 -0800

    Unfinished Work. Feel free to complete it.

--- gameCore.py
+++ gameCore.py
@@ -8,17 +8,15 @@
 #
 # Do it EXACTLY as shown above. Copy and paste if you need to.
 
+# This file basically pulls all the modules together.
+
 import OSCheck
-import random
-import time
 from gameStartTest import gameStart
+from playerInput import *
 import inventory
 import Store
 import gameMove
 
 
 gameStart()
-corNumberX = 5
-corNumberY = 1
-#This is from 'gameMove.py'
 playerAction()
